ab8fb9b25c8d9e1f2b9376907968c3c49da91914
Yoshiaki Itakura <itathirdchildnegi@gmail.com>
2018-03-15 18:42:08 +0900

    update dependencies
    update dependencies

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -11,26 +11,36 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('bundle.css'),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.ProvidePlugin({ riot: 'riot' })
   ],
   module: {
-    preLoaders: [
-      { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs', query: { type: 'babel' } }
-    ],
-    loaders: [
-      { test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.css$|\.styl$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss!stylus') },
-      { test: /\.svg$/, exclude: /node_modules/, loader: 'file?name=[name].[ext]' }
+    rules: [
+      { test: /\.tag$/, enforce: 'pre', exclude: /node_modules/, loader: 'riotjs-loader', options: { type: 'babel' } },
+      { test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$|\.styl$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('autoprefixer')({
+                    browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4','ios_saf >= 8']
+                  })
+                ]
+              }
+            },
+            'stylus-loader'] }) },
+      { test: /\.svg$/, exclude: /node_modules/, loader: 'file-loader?name=[name].[ext]' }
     ]
   },
-  postcss: () => {
-    return [autoprefixer({ browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4','ios_saf >= 8'] })];
-  },
   resolve: {
-      extensions: ['', '.js', '.tag', '.styl']
+    extensions: ['*', '.js', '.tag', '.styl']
   }
 }
